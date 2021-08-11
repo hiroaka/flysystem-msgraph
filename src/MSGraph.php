1dc61bee6bda250da7c1a9882c9132d99385da6a
Renan R Praxedes <renan.praxedes@gmail.com>
2021-08-11 16:49:16 -0300

    large upload

--- src/MSGraph.php
+++ src/MSGraph.php
@@ -2,8 +2,10 @@
 
 namespace ProcessMaker\Flysystem\Adapter;
 
+use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Psr7\Stream;
+use GuzzleHttp\Psr7\Utils;
 use Illuminate\Filesystem\FilesystemAdapter;
 use InvalidArgumentException;
 use League\Flysystem\Adapter\AbstractAdapter;
@@ -17,6 +19,7 @@ use ProcessMaker\Flysystem\Adapter\MSGraph\SiteInvalidException;
 use League\OAuth2\Client\Provider\GenericProvider;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use Microsoft\Graph\Graph;
+use Microsoft\Graph\Http\GraphResponse;
 use Microsoft\Graph\Model;
 
 class MSGraph extends FilesystemAdapter
@@ -39,13 +42,22 @@ class MSGraph extends FilesystemAdapter
     // Our url prefix to be used for most file operations. This gets created in our constructor
     private $prefix;
 
+    private $chunkSize;
+    /** @var int $largeFileUploadProgress */
+    private $largeFileUploadProgress;
+    /** @var int $currentLargeFileRetryInterval */
+    private $currentLargeFileRetryInterval;
+    // If we get a server error, retry for a maximum of 10 times in the intervals below (in seconds)
+    private $retryIntervals = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
+
+
     public function __construct($token, $mode = self::MODE_ONEDRIVE, $targetId, $driveName = null)
     {
         if ($mode != self::MODE_ONEDRIVE && $mode != self::MODE_SHAREPOINT) {
             throw new ModeException("Unknown mode specified: " . $mode);
         }
         $this->mode = $mode;
-
+        $this->largeFileUploadProgress = 0;
 
         //Aplication should take care of token and refresh it
 
@@ -368,6 +380,164 @@ class MSGraph extends FilesystemAdapter
     }
 
 
+
+    /**
+     * Upload a file that's larger than 4MB.
+     */
+    public function uploadLargeItem(string $path, string $filesrc): ?Model\DriveItem
+    {
+        // Optimal chunk size is 5-10MiB and should be a multiple of 320 KiB
+        $chunkSizeBytes = 10485760; // is 10 MiB
+        $fileSize = filesize($filesrc);
+        $options = [
+            'item' => ['@microsoft.graph.conflictBehavior' => 'rename']
+        ];
+
+        // Get the URL that we can post our file chunks to.
+        $createItemUrl = '/me/drive/root:/' . $path . ':/createUploadSession';
+        $session = $this->graph->createRequest("POST", $createItemUrl)
+            ->setReturnType(Model\UploadSession::class)
+            ->attachBody($options)
+            ->execute();
+        $uploadUrl = $session->getUploadUrl();
+
+        // Upload the various chunks.
+        // $status will be false until the process is complete.
+        $status = false;
+        $handle = fopen($filesrc, "rb");
+        while (!$status && !feof($handle)) {
+            $chunk = fread($handle, $chunkSizeBytes);
+            $status = $this->nextChunk($uploadUrl, $fileSize, $chunk);
+        }
+
+        // The final value of $status will be the data from the API for the object
+        // that has been uploaded.
+        $result = false;
+        if ($status !== false) {
+            /** @var Model\DriveItem */
+            $result = $status;
+        }
+
+        fclose($handle);
+
+        if (!$result) {
+            return null;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Send the next part of the file to upload.
+     * @param [$chunk] the next set of bytes to send. If false will used $data passed
+     * at construct time.
+     *
+     * Got inspiration from:
+     * https://github.com/googleapis/google-api-php-client/blob/master/src/Google/Http/MediaFileUpload.php#L113-L141
+     */
+    private function nextChunk(string $uploadUrl, int $fileSize, $chunk = false)
+    {
+        if (false == $chunk) {
+            $chunk = substr(null, $this->largeFileUploadProgress, $this->chunkSize);
+        }
+        $lastBytePos = $this->largeFileUploadProgress + strlen($chunk) - 1;
+        $headers = array(
+            'Content-Range' => "bytes $this->largeFileUploadProgress-$lastBytePos/$fileSize",
+            'Content-Length' => strlen($chunk),
+        );
+
+//        dd($headers);
+
+        /**
+         * We shouldn't send the Authorization header here (as a tempauth token is included in the $uploadUrl),
+         * so we can use a plain GuzzleHttp client.
+         */
+        $client = new Client();
+        $response = $client->request(
+            "PUT",
+            $uploadUrl,
+            [
+                'headers' => $headers,
+                'body' => Utils::streamFor($chunk),
+                'timeout' => 90
+            ]
+        );
+
+        // A 404 code indicates that the upload session no longer exists, thus requiring us to start all over.
+        if ($response->getStatusCode() === 404) {
+            throw new \Exception('Upload URL has expired, please create new upload session');
+        }
+
+        // Retry if we get a server error, for a maximum of 10 times with time intervals.
+        if (in_array($response->getStatusCode(), [500, 502, 503, 504])) {
+            if ($this->currentLargeFileRetryInterval > 9) {
+                throw new \Exception('Upload failed after 10 attempts.');
+            }
+            // Wait for the amount of seconds defined in the retryIntervals.
+            sleep($this->retryIntervals[$this->currentLargeFileRetryInterval]);
+            $this->currentLargeFileRetryInterval++;
+            $this->nextChunk($uploadUrl, $fileSize, $chunk);
+        }
+
+        /**
+         * If we have uploaded the last chunk, we should receive a 200 or 201 Created response code,
+         * including a DriveItem. We use the Graph function getResponseAsObject to get the DriveItem object.
+         */
+        if (($fileSize - 1) == $lastBytePos) {
+            /**
+             * If a conflict occurs after the file is uploaded (for example,
+             * an item with the same name was created during the upload session),
+             * an error is returned when the last byte range is uploaded.
+             */
+            if ($response->getStatusCode() === 409) {
+                throw new \Exception(
+                    'File already exists. A file with the same name might have been created during the upload session.'
+                );
+            }
+
+            if (in_array($response->getStatusCode(), [200, 201])) {
+                $response = new GraphResponse(
+                    $this->graph->createRequest('', ''),
+                    $response->getBody(),
+                    $response->getStatusCode(),
+                    $response->getHeaders()
+                );
+
+                $item = $response->getResponseAsObject(Model\DriveItem::class);
+                return $item;
+            }
+
+            throw new \Exception(
+                'Unknown error occured while uploading last part of file. HTTP response code is '
+                . $response->getStatusCode()
+            );
+        }
+
+        /**
+         * If we didn't receive a 202 Accepted response from the Graph API, something has gone wrong.
+         */
+        if ($response->getStatusCode() !== 202) {
+            throw new \Exception(
+                'Unknown error occured while trying to upload file chunk. HTTP status code is '
+                . $response->getStatusCode()
+            );
+        }
+
+        /**
+         * If we received a 202 Accepted response, it will include a nextExpectedRanges key, which will tell
+         * us the next range we'll upload.
+         */
+        $body = json_decode($response->getBody()->getContents(), true);
+        $nextExpectedRanges = $body['nextExpectedRanges']; // e.g. ["12345-55232","77829-99375"]
+        $nextRange = $nextExpectedRanges[0]; // e.g. "12345-55232"
+        $nextRangeExploded = explode('-', $nextRange); // e.g. ["12345", "55232"]
+
+        $this->largeFileUploadProgress = $nextRangeExploded[0];
+
+        // Upload not complete yet, return false.
+        return false;
+    }
+
     // Write methods
     public function write($path, $contents, array $config = [])
     {
@@ -376,6 +546,7 @@ class MSGraph extends FilesystemAdapter
         try {
             $driveItem = $this->graph->createRequest('PUT', '/me/drive/root:/' . $path . ':/content')
                 ->attachBody($contents)
+                ->addHeaders(isset($config['content-type']) ? array("Content-Type" => $config['content-type']) : [])
                 ->setReturnType(Model\DriveItem::class)
                 ->execute();
             // Successfully created
@@ -476,7 +647,9 @@ class MSGraph extends FilesystemAdapter
 
     public function putStream($path, $resource, array $config = [])
     {
-        $this->write($path, $resource, $config);
+//        $this->write($path, $resource, $config);
+        $this->uploadLargeItem($path, $resource);
+
 //        // Attempt to write to onedrive
 //        try {
 //            $driveItem = $this->graph->createRequest('PUT', $this->prefix . 'root:/' . $path . ':/content')
